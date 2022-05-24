0d46499b99f7a1eada543664a28a6d942d210291
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-24 15:50:24 -0500

    Update Import.sol

--- Import.sol
+++ Import.sol
@@ -2,8 +2,10 @@
 pragma solidity ^ 0.8.14;
 
 // * import { symbol1 as alias, symbol2 } from "filename";
-import { Unauthorized, helper } from "./Helper.sol";
+import { Unauthorized, helper as h1 } from "./Helper.sol";
+
+function helper(uint256 x) view returns (uint256){
+}
 
 contract Import{
-
 }
