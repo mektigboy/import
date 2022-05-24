ce00510c58f9450e831b8c9c07c0e4dc176cdc67
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-24 15:25:35 -0500

    Update Helper.sol

--- Helper.sol
+++ Helper.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^ 0.8.14;
 
+error Unauthorized();
+
+// Functions outside contract.
+function helper(uint256 x) view returns (uint256) {
+  return x * 2;
+}
+
 contract Helper {
   function test() external view returns (uint256) {
     return helper(123);
