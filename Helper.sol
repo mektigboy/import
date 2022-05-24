b7fcc65a97852c0bbf435871817e7eb465384859
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-24 15:50:35 -0500

    Update Helper.sol

--- Helper.sol
+++ Helper.sol
@@ -4,12 +4,12 @@ pragma solidity ^ 0.8.14;
 error Unauthorized();
 
 // Functions outside contract.
-function helper(uint256 x) view returns (uint256) {
+function helper(uint256 x) pure returns (uint256) {
   return x * 2;
 }
 
 contract Helper {
-  function test() external view returns (uint256) {
+  function test() external pure returns (uint256) {
     return helper(123);
   }
 }
