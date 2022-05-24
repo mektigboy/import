32520ecd259b17dc0af7501aa67c9130872cb9fb
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-24 15:16:09 -0500

    Update Import.sol

--- Import.sol
+++ Import.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^ 0.8.14;
 
+// * import { symbol1 as alias, symbol2 } from "filename";
 import { Unauthorized, helper } from "CustomError";
 
 contract Import{
